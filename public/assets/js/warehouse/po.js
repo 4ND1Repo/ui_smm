9877ec6a9969353486b908560732b53ab65b5097
Andi Fauzi Akbar <faujiakbar@gmail.com>
2019-09-11 11:37:12 +0700

    forget when add po

--- public/assets/js/warehouse/po.js
+++ public/assets/js/warehouse/po.js
@@ -357,6 +357,7 @@ $(document).ready(function(){
         tmpHtml += '</div>';
         // input qty
         tmpHtml += '<div class="text-center">-</div>';
+        tmpHtml += '<div class="text-center">-</div>';
         tmpHtml += '<div><input type="text" class="form-control form-control-sm qtyPO" name="data['+map[selection].id+']" placeholder="Kuantiti" value="'+map[selection].need+'"></div>';
         tmpHtml += '<div class="text-center">'+data[5]+'</div>';
         tmpHtml += '<div style="position:relative; overflow:visible;"><input type="text" class="form-control form-control-sm" name="notes['+map[selection].id+']" placeholder="Keterangan"></div>';
